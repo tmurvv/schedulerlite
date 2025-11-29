ac7a18fb1658496fdb850807c72daa8f763ac1e3
Tisha Murvihill <tmurv@shaw.ca>
2025-11-29 13:15:38 -0700

    fix install cdk cli

--- apps/backend/timesheets/src/hello/handler.ts
+++ apps/backend/timesheets/src/hello/handler.ts
@@ -1,10 +1,7 @@
-import { APIGatewayProxyHandlerV2 } from "aws-lambda";
-
-export const handler: APIGatewayProxyHandlerV2 = async () => {
-  const message = "Hello from SchedulerLite";
-  console.log(message);
+export async function handler(event: any) {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message }),
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ message: `Hello from SchedulerLite v.0.0.0 ${event.message}` })
   };
-};
+}
--- apps/frontend/timesheets/src/App.tsx
+++ apps/frontend/timesheets/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Hello from SchedulerLite FE v0.0.0
         </p>
       </div>
       <p className="read-the-docs">
